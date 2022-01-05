4486b51e810d267cd8b468d789f8055ae8d057a0
Wouter127 <r0785520@student.thomasmore.be>
2022-01-05 13:37:28 +0100

    get combo region

--- src/main/java/com/example/edgeservice/controller/FilledGrapeWineController.java
+++ src/main/java/com/example/edgeservice/controller/FilledGrapeWineController.java
@@ -5,9 +5,15 @@ import com.example.edgeservice.model.Grape;
 import com.example.edgeservice.model.Wine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilledGrapeWineController {
     @Autowired
@@ -29,4 +35,24 @@ public class FilledGrapeWineController {
 
         return new FilledGrapeWine(grape, wine);
     }
+
+    @GetMapping("/combo/region/{region}")
+    public List<FilledGrapeWine> getWinesAndGrapesByRegion(@PathVariable String region){
+        List<FilledGrapeWine> returnList = new ArrayList<>();
+
+        ResponseEntity<List<Wine>> responseEntityWines =
+                restTemplate.exchange("http://" + wineServiceBaseUrl + "/wines/region/{region}",
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Wine>>() {
+                        }, region);
+        List<Wine> wines = responseEntityWines.getBody();
+        for (Wine wine: wines) {
+            Grape grape =
+                    restTemplate.getForObject("http://" + grapeServiceBaseUrl + "/grapes/grapename/{grapeName}",
+                            Grape.class, wine.getGrapeName());
+            returnList.add(new FilledGrapeWine(grape, wine));
+        }
+
+        return returnList;
+    }
+
 }
