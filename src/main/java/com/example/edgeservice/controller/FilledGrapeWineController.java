c1c72bd7f20b901e5c2d9e4860a8e1daa6d3af7e
Stef-Van-Gool <r0740461@student.thomasmore.be>
2022-01-05 13:37:14 +0100

    2 GET methods

--- src/main/java/com/example/edgeservice/controller/FilledGrapeWineController.java
+++ src/main/java/com/example/edgeservice/controller/FilledGrapeWineController.java
@@ -5,9 +5,15 @@ import com.example.edgeservice.model.Grape;
 import com.example.edgeservice.model.Wine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilledGrapeWineController {
     @Autowired
@@ -19,14 +25,32 @@ public class FilledGrapeWineController {
     @Value("${grapeservice.baseurl}")
     private String grapeServiceBaseUrl;
 
-    @GetMapping("/wines/{grapeName}")
-    public FilledGrapeWine getWinesByGrape(@PathVariable String grapeName){
-        Grape grape = restTemplate.getForObject("http://" + grapeServiceBaseUrl + "/grapes/grapename/{grapeName}",
-                Grape.class, grapeName);
-
-        Wine wine = restTemplate.getForObject("http://" + wineServiceBaseUrl + "/wines/grape/" + grapeName,
+    @GetMapping("/combo/wine/{name}")
+    public FilledGrapeWine getGrapeByWine(@PathVariable String name){
+        Wine wine = restTemplate.getForObject("http://" + wineServiceBaseUrl + "/wines/name/{name}" + name,
                 Wine.class);
 
+        Grape grape = restTemplate.getForObject("http://" + grapeServiceBaseUrl + "/grapes/grapename/{grapeName}",
+                        Grape.class, wine.getGrapeName());
         return new FilledGrapeWine(grape, wine);
+    }
+
+    @GetMapping("/combo/country/{country}")
+    public List<FilledGrapeWine> getWinesAndGrapesByCountry(@PathVariable String country){
+        List<FilledGrapeWine> returnList = new ArrayList<>();
+
+        ResponseEntity<List<Wine>> responseEntityWines =
+                restTemplate.exchange("http://" + wineServiceBaseUrl + "/wines/country/{country}",
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Wine>>() {
+                        }, country);
+        List<Wine> wines = responseEntityWines.getBody();
+        for (Wine wine: wines) {
+            Grape grape =
+                    restTemplate.getForObject("http://" + grapeServiceBaseUrl + "/grapes/grapename/{grapeName}",
+                            Grape.class, wine.getGrapeName());
+            returnList.add(new FilledGrapeWine(grape, wine));
+        }
+
+        return returnList;
     }
 }
